88e20afd9997bb6ccd52f1915da20a93468c6b22
remy bouteloup <remy.bouteloup@yahoo.fr>
2015-01-26 14:03:47 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+float flottant1, flottant2, resultat;
+
+cout << "Entrez le premier flottant : ";
+cin >> flottant1;
+
+cout << "Entrez le deuxième flottant : ";
+cin >> flottant2;
+
+resultat = flottant1/flottant2;
+
+cout << "Le résultat est : " << resultat << endl;
+
+}
