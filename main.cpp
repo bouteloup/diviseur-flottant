0edbd8be3e35300b5197f10906fe6a45ab8828a4
remy bouteloup <remy.bouteloup@yahoo.fr>
2015-01-26 14:51:08 +0100

    fonctionnel

--- main.cpp
+++ main.cpp
@@ -9,8 +9,10 @@ float flottant1, flottant2, resultat;
 cout << "Entrez le premier flottant : ";
 cin >> flottant1;
 
+do{
 cout << "Entrez le deuxième flottant : ";
 cin >> flottant2;
+} while (flottant2 == 0);
 
 resultat = flottant1/flottant2;
 
